cc51207881be9c76690bcbc1ce14d048ecc71d76
Sam Kingston <sam@sjkwi.com.au>
2014-01-16 12:14:09 +1000

    Fix bug where shipping wasn't included in payment amount with cartridge handler

--- commweb/cartridge_hook.py
+++ commweb/cartridge_hook.py
@@ -1,14 +1,12 @@
 from cartridge.shop.checkout import CheckoutError
-from cartridge.shop.models import Cart
 
 from commweb.exc import PaymentDeclinedError
 from commweb.purchase import Purchase
 
 def cartridge_payment_handler(request, order_form, order):
-    cart = Cart.objects.from_request(request)
     trans_id = 'WFS_%d' % order.id
 
-    p = Purchase(cart.total_price(), trans_id,
+    p = Purchase(order.total, trans_id,
             order_form.cleaned_data['card_number'],
             order_form.cleaned_data['card_expiry_year'][2:4],
             order_form.cleaned_data['card_expiry_month'],
